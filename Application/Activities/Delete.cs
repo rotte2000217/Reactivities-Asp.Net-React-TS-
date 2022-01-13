c1c0b5d2b4c0448f7b07d547adf78a6e86c64edf
ibrag <ibragimov-amir@list.ru>
2022-01-13 21:06:47 +0600

    Added AutoMapper NuGet Package Delete.cs class For delete Query CRUD operations ApplicationServiceExtensions.cs which collect all services in StartUp.cs class

--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -1,36 +1,33 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
-using AutoMapper;
 using Domain;
 using MediatR;
-using Microsoft.EntityFrameworkCore.Storage;
 using Persistence;
 
 namespace Application.Activities
 {
-    public class Edit
+    public class Delete
     {
         public class Command : IRequest
         {
-            public Activity Activity { get; set; }
+            public Guid Id { get; set; }
         }
 
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            private readonly IMapper _mapper;
 
-            public Handler(DataContext context, IMapper mapper)
+            public Handler(DataContext context)
             {
                 _context = context;
-                _mapper = mapper;
             }
             
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
-                var activity = await _context.Activities.FindAsync(request.Activity.Id);
+                var activity = await _context.Activities.FindAsync(request.Id);
 
-                _mapper.Map(request.Activity, activity);
+                _context.Remove(activity);
 
                 await _context.SaveChangesAsync();
 
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
@@ -15,7 +17,6 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
-            
             public Handler(DataContext context)
             {
                 _context = context;
@@ -23,7 +24,7 @@ namespace Application.Activities
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await _context.Activities.ToListAsync();
+                return await _context.Activities.ToListAsync(cancellationToken);
             }
         }
     }
