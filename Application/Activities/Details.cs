c28b30c36e862167c52cf1bb81ae1d85d93e5641
ibrag <ibragimov-amir@list.ru>
2022-01-23 15:02:13 +0600

    added Validator for error handling For create requests

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -25,7 +25,11 @@ namespace Application.Activities
             
             public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await _context.Activities.FindAsync(request.Id);
+                var activity = await _context.Activities.FindAsync(request.Id);
+
+                if (activity == null) throw new Exception("Activity not found");
+
+                return activity;
             }
         }
     }
