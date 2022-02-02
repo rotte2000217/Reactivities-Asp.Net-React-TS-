0f944815b1cc94746fc128b975bd38be65ce0702
ibrag <ibragimov-amir@list.ru>
2022-02-02 14:50:18 +0600

    added Following migrations

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -9,6 +9,7 @@ namespace Domain
         public string Bio { get; set; }
         public ICollection<ActivityAttendee> Activities { get; set; }
         public ICollection<Photo> Photos { get; set; }
-
+        public virtual ICollection<UserFollowing> Followings { get; set; }
+        public virtual ICollection<UserFollowing> Followers { get; set; }
     }
 }           
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -17,7 +17,7 @@ namespace Persistence
         public DbSet<Photo> Photos { get; set; }
         
         public DbSet<Comment> Comments { get; set; }
-
+        public DbSet<UserFollowing> UserFollowings { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
@@ -38,6 +38,21 @@ namespace Persistence
                 .HasOne(a => a.Activity)
                 .WithMany(c => c.Comments)
                 .OnDelete(DeleteBehavior.Cascade);
+            
+            builder.Entity<UserFollowing>(b =>
+            {
+                b.HasKey(k => new { k.ObserverId, k.TargetId });
+
+                b.HasOne(o => o.Observer)
+                    .WithMany(f => f.Followings)
+                    .HasForeignKey(o => o.ObserverId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                b.HasOne(o => o.Target)
+                    .WithMany(f => f.Followers)
+                    .HasForeignKey(o => o.TargetId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
         }
     }
 }
